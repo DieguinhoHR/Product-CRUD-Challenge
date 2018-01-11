e8550cab485b57a3cc087c30d42a3eaae93a99f7
diego <diego_henriquerodrigues@hotmail.com>
2018-01-11 13:30:47 -0200

    Add method index to controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = Product::all();
+
+        return $this->response()->json($products);
+    }
 }
